c7ab88f0a3f1ac536369cde2f5c47158eaedb7bf
Jesus Rafael <jesusrafaellh@gmail.com>
2018-05-23 00:21:48 -0400

    Add babel & webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path =  require('path');
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+    context: path.join(__dirname, 'src'),
+    entry: [
+        './index.js'
+    ], 
+    output: {
+        path: path.join(__dirname, 'www'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, 'node_modules')
+        ]
+    }
+};
