f780003950725b25003a0f689005bf65be9a8611
Jesus Rafael <jesusrafaellh@gmail.com>
2018-05-23 23:13:33 -0400

    MEARN APP base de React-Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('HELLO WORD');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    constructor () {
+        super();
+        this.state = {
+            name: '',
+            lastName: ''
+        };
+    }
+
+    handleName (e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    handleLastName (e) {
+        this.setState({
+            lastName: e.target.value
+        });
+    }
+
+    render () {
+        return (
+            <div >
+              <h4>Hello Word from React</h4>
+              <div>
+                  <label>Name:</label>
+                  <input type="text" onChange={e => this.handleName(e)} />
+                  <label>Apellido</label>
+                  <input type="text" onChange={e => this.handleLastName(e)}/>
+              </div>
+              <div>
+                 {this.state.name} {this.state.lastName}
+              </div>
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
